b6a89518c87606ff127c19e230e965106e0b5eb8
David Vogel <Dadido3@aol.com>
2020-12-18 14:48:21 +0100

    Add triangle creation and handling

--- lua/d3bot/sh_navmesh/navmesh.lua
+++ lua/d3bot/sh_navmesh/navmesh.lua
@@ -3,6 +3,7 @@ AddCSLuaFile()
 local D3bot = D3bot
 local NAV_MESH = D3bot.NAV_MESH
 local NAV_EDGE = D3bot.NAV_EDGE
+local NAV_TRIANGLE = D3bot.NAV_TRIANGLE
 
 -- Get new instance of a navmesh container object.
 -- This contains edges and triangles of a navmesh and provides methods for location and path finding.
@@ -18,7 +19,7 @@ function NAV_MESH:New()
 end
 
 -- Will return the edge that is built with the two given points, if there is one.
-function NAV_MESH:FindEdge(p1, p2)
+function NAV_MESH:FindEdge2P(p1, p2)
 	for _, edge in ipairs(self.Edges) do
 		if edge:ConsistsOfPoints(p1, p2) then return edge end
 	end
@@ -27,11 +28,46 @@ function NAV_MESH:FindEdge(p1, p2)
 end
 
 -- Will create a new edge with the given two points, or return an already existing edge.
-function NAV_MESH:FindOrCreateEdge(p1, p2)
+function NAV_MESH:FindOrCreateEdge2P(p1, p2)
 	local edge = self:FindEdge(p1, p2)
 	if edge then return edge end
 
 	local newEdge = NAV_EDGE:New(p1, p2)
+	if not newEdge then return end
 	table.insert(self.Edges, newEdge)
 	return newEdge
+end
+
+-- Will return the triangle that is built with the three given points, if there is one.
+function NAV_MESH:FindTriangle3P(p1, p2, p3)
+	local e1, e2, e3 = self:FindEdge(p1, p2), self:FindEdge(p2, p3), self:FindEdge(p3, p1)
+
+	return self:FindTriangle3E(e1, e2, e3)
+end
+
+-- Will return the triangle that is built with the three given edges, if there is one.
+function NAV_MESH:FindTriangle3E(e1, e2, e3)
+	for _, triangle in ipairs(self.Triangles) do
+		if triangle:ConsistsOfEdges(e1, e2, e3) then return triangle end
+	end
+
+	return
+end
+
+-- Will create a new triangle with the given three points, or return an already existing triangle.
+function NAV_MESH:FindOrCreateTriangle3P(p1, p2, p3)
+	local e1, e2, e3 = self:FindOrCreateEdge2P(p1, p2), self:FindOrCreateEdge2P(p2, p3), self:FindOrCreateEdge2P(p3, p1)
+
+	return self:FindOrCreateTriangle3E(e1, e2, e3) -- TODO: When create triangle fails, some of the edges may not be used. Prevent this, or GC them later
+end
+
+-- Will create a new triangle with the given three edges, or return an already existing triangle.
+function NAV_MESH:FindOrCreateTriangle3E(e1, e2, e3)
+	local triangle = self:FindTriangle3E(e1, e2, e3)
+	if triangle then return triangle end
+
+	local newTriangle = NAV_TRIANGLE:New(e1, e2, e3)
+	if not newTriangle then return end
+	table.insert(self.Triangles, newTriangle)
+	return newTriangle
 end
--- lua/d3bot/sh_navmesh/triangle.lua
+++ lua/d3bot/sh_navmesh/triangle.lua
@@ -11,7 +11,22 @@ function NAV_TRIANGLE:New(e1, e2, e3)
 		Edges = {e1, e2, e3}
 	}
 
+	-- TODO: Selfcheck
+
 	setmetatable(obj, self)
 	self.__index = self
 	return obj
+end
+
+-- Returns wether the triangle consists out of the three given edges or not.
+function NAV_EDGE:ConsistsOfEdges(e1, e2, e3)
+	-- There is probably a nicer way to do this, but it doesn't need to be that fast
+	-- In the worst case
+	if self.Edges[1] == e1 and self.Edges[2] == e2 and self.Edges[3] == e3 then return true end
+	if self.Edges[1] == e1 and self.Edges[2] == e3 and self.Edges[3] == e2 then return true end
+	if self.Edges[1] == e2 and self.Edges[2] == e1 and self.Edges[3] == e3 then return true end
+	if self.Edges[1] == e2 and self.Edges[2] == e3 and self.Edges[3] == e1 then return true end
+	if self.Edges[1] == e3 and self.Edges[2] == e1 and self.Edges[3] == e2 then return true end
+	if self.Edges[1] == e3 and self.Edges[2] == e2 and self.Edges[3] == e1 then return true end
+	return false
 end
