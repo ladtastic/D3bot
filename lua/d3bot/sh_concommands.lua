d49901a27a0872019f5463670550d5489c9dbc88
David Vogel <Dadido3@aol.com>
2020-12-23 15:47:01 +0100

    Fix concommands

--- lua/d3bot/sh_concommands.lua
+++ lua/d3bot/sh_concommands.lua
@@ -5,7 +5,7 @@ local NAV_FILE = D3bot.NavFile
 
 -- Give a player the navmeshing SWEP.
 CONCOMMANDS.EditMesh = CONCOMMAND:New("d3bot_editmesh")
-function CONCOMMANDS.Test:OnServer(ply, cmd, args, argStr)
+function CONCOMMANDS.EditMesh:OnServer(ply, cmd, args, argStr)
 	if not ply:IsSuperAdmin() then return end
 
 	ply:Give("weapon_d3_navmesher")
@@ -13,7 +13,7 @@ end
 
 -- Save the main navmesh to disk.
 CONCOMMANDS.SaveMesh = CONCOMMAND:New("d3bot_savemesh")
-function CONCOMMANDS.Test:OnServer(ply, cmd, args, argStr)
+function CONCOMMANDS.SaveMesh:OnServer(ply, cmd, args, argStr)
 	if not ply:IsSuperAdmin() then return end
 
 	NAV_FILE.SaveMainNavmesh()
@@ -21,7 +21,7 @@ end
 
 -- Load the main navmesh from disk.
 CONCOMMANDS.LoadMesh = CONCOMMAND:New("d3bot_loadmesh")
-function CONCOMMANDS.Test:OnServer(ply, cmd, args, argStr)
+function CONCOMMANDS.LoadMesh:OnServer(ply, cmd, args, argStr)
 	if not ply:IsSuperAdmin() then return end
 
 	NAV_FILE.LoadMainNavmesh()
